346bd10004c5991075064facd4accc5025f01081
rlatjddbs <sykk18@naver.com>
2023-05-20 23:56:19 +0900

    -

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/sykim/DC
 
-/home/sykim/anaconda3/envs/diffuser/bin/python train.py --dataset maze2d-umaze-v1 --max_path_length 250
+/home/sykim/anaconda3/envs/diffuser/bin/python train.py --dataset walker2d-medium-expert-v2 --max_path_length 1000
                                             
