b34470032a0fc07bfb392b8c6e20bcce9944a284
rlatjddbs <sykk18@naver.com>
2023-05-23 16:15:37 +0900

    Fixed temporal.py

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/sykim/DC
 
-/home/sykim/anaconda3/envs/diffuser/bin/python train.py --dataset maze2d-umaze-v1 --max_path_length 250
+/home/sykim/anaconda3/envs/diffuser/bin/python train.py --dataset maze2d-large-v1 --max_path_length 600
                                             
