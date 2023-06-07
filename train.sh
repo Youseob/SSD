11fb661327a39cd7c26b20c66bb3902e272d76dc
rlatjddbs <sykk18@naver.com>
2023-06-07 14:34:40 +0900

    -

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 cd /home/sykim/DC
 source ~/.bashrc
 
-D4RL_DATASET_DIR=/tmp /home/sykim/anaconda3/envs/diffuser/bin/python3 train.py --cid $1 --pid $2 --dataset FetchPickAndPlace-v1 --max_path_length 50
+D4RL_DATASET_DIR=/tmp /home/sykim/anaconda3/envs/diffuser/bin/python3 train.py --cid $1 --pid $2 --dataset FetchPush-v1 --max_path_length 50
 #D4RL_DATASET_DIR=/tmp /home/sykim/anaconda3/envs/diffuser/bin/python3 train.py --dataset maze2d-large-v1 --max_path_length 600
 
                                             
